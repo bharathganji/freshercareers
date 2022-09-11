116640a873f0ea6e6d430c4f97d1a03afe21bae2
Bharath Ganji <bharath_ganji@epam.com>
2022-09-11 08:28:48 +0530

    added data.json path

--- src/app/client/modules/components/search/search.component.ts
+++ src/app/client/modules/components/search/search.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { UntypedFormBuilder, UntypedFormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 
 @Component({
@@ -8,9 +8,9 @@ import { Router } from '@angular/router';
   styleUrls: ['./search.component.css'],
 })
 export class SearchComponent implements OnInit {
-  registerForm!: FormGroup;
+  registerForm!: UntypedFormGroup;
 
-  constructor(private router: Router, private fb: FormBuilder) {}
+  constructor(private router: Router, private fb: UntypedFormBuilder) {}
   ngOnInit(): void {
     this.registerForm = this.fb.group({
       username: ['', Validators.required],
@@ -22,7 +22,7 @@ export class SearchComponent implements OnInit {
     });
   }
 
-  onSubmit(form: FormGroup) {
+  onSubmit(form: UntypedFormGroup) {
     console.log(' is registeration form Valid?', form.valid); // true or false
     console.log('entered search: ', form.value.username);
     if (form.valid) {
