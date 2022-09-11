bdae2ff9107c001bac6513716b1aaed07b2cedb4
Bharath Ganji <bharath_ganji@epam.com>
2022-09-11 16:48:44 +0530

    search functionality added

--- src/app/client/modules/components/search-posts/search-posts.component.ts
+++ src/app/client/modules/components/search-posts/search-posts.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Query } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { SupabaseService } from 'src/app/shared/supabase.service';
+import { data_job } from '../jobs/data';
 
 @Component({
   selector: 'app-search-posts',
@@ -10,20 +11,42 @@ import { SupabaseService } from 'src/app/shared/supabase.service';
 export class SearchPostsComponent implements OnInit {
   constructor(
     private router: ActivatedRoute,
+    private router1: Router,
+
     private supabaseService: SupabaseService
   ) {}
 
+  temp1: any = data_job;
+
   QueryArray!: any;
+
+  jobcollection!:any;
+  isLoading = false;
+
+
   ngOnInit(): void {
     this.router.queryParams.subscribe((res) => {
       let temp = { ...res }; //will give query params as an object
-      
       this.QueryArray = temp['searchInput'].trim().split(' ');
-      // console.log(String( this.QueryArray));
-
-      // console.log(this.supabaseService.searchFilter(this.QueryArray));
-      // console.log(this.supabaseService.searchFilter(String( this.QueryArray[0])));
-
+  
+      let querystring = String(this.QueryArray).toLowerCase();
+      let filterdata = this.temp1.filter((ele: any) => this.filterFun(ele,querystring));
+      this.jobcollection=filterdata
     });
+  }
+  filterFun(ele: any,query: string) {
+    if (ele[0].toLowerCase().includes(query.toLowerCase())) {
+      return ele;
+    }
+  }
+
+
+  
+  p: number = 1;
+
+  public gotoPost(url: any, id: any): void {
+    // console.log(id);
+    id=JSON.stringify(id);
+    this.router1.navigate([url, id]);
   }
 }
