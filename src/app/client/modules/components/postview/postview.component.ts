57bb78373a1bdf85613e2b38ae32d9cd0f91d491
Bharath Ganji <bharath_ganji@epam.com>
2022-10-01 10:40:40 +0530

    added meta tags for each post using angular meta service

--- src/app/client/modules/components/postview/postview.component.ts
+++ src/app/client/modules/components/postview/postview.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { jobData } from 'src/app/classes/jobData';
 import { SupabaseService } from 'src/app/shared/supabase.service';
+import { Meta, Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-postview',
@@ -12,17 +13,20 @@ export class PostviewComponent implements OnInit {
   constructor(
     private router: Router,
     private supabaseService: SupabaseService,
-    private activateRoute: ActivatedRoute
+    private activateRoute: ActivatedRoute,
+    private titleService: Title,
+    private metaTagService: Meta
   ) {}
   // post!: jobData;
   post!: any;
   // id!: string | null;
 
   url!: any;
-  date!:any;
+  date!: any;
 
   isLoading = true;
   ngOnInit(): void {
+   
     // this.activateRoute.paramMap.subscribe((param) => {
     //   this.supabaseService
     //     .fetchByID(param.get('id'))
@@ -36,21 +40,32 @@ export class PostviewComponent implements OnInit {
       // this.post= param.get('id')
       this.post = JSON.parse(param.get('id'));
       console.log(this.post);
-      
 
       this.date = this.post.pop().split(' ');
       this.url = this.post.pop();
       this.detectURLs(this.url);
 
       this.isLoading = false;
+
+
+      this.titleService.setTitle("Example of SEO Meta Tag using Angular");  
+      this.metaTagService.addTags([  
+        { name: 'keywords', content: this.post.toString()  },  
+        { name: 'robots', content:  this.post.toString()},  
+        { name: 'title', content:  this.post.toString() },  
+        { name: 'position', content:  this.post.toString() },  
+        { name: 'location', content:  this.post.toString() },  
+        { charset: 'UTF-8' }  
+      ]);  
     });
+   
   }
 
   link!: any;
   applynow() {
     window.open(this.link, '_blank');
   }
- 
+
   // applynow(link: any) {
   //   window.open(link, '_blank');
   // }
